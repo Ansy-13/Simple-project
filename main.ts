14e715d6879f643d52be6a0289856a90ab24823e
Ansy-13 <aimahadia13@gmail.com>
2024-04-28 18:01:27 +0500

    this is my simple project

---
+++ main.ts
@@ -0,0 +1,17 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+
+let myAge = 18
+
+
+
+let answer = await inquirer.prompt([{message: "please give your age" , type: "number" , name: "value"}])
+if(answer.value > myAge){
+    console.log("you are eligibale for driving")
+}else{
+    console.log("you are not eligiable")
+}
+
+
+
